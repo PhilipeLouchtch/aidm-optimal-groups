b1870c038e194556b32713583f4b8a4f21535d64
Philipe Louchtch <philipelouchtch@outlook.com>
2022-03-31 13:58:00 +0200

    add helpful (but unused) methods to Matching

--- src/main/java/nl/tudelft/aidm/optimalgroups/model/matching/Matching.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/model/matching/Matching.java
@@ -20,4 +20,26 @@ public interface Matching<FROM, TO>
 	 * @return The source dataset
 	 */
 	DatasetContext datasetContext();
+	
+	/**
+	 * @param from
+	 * @return  All matches that are matched to the given FROM
+	 */
+	default List<Match<FROM, TO>> allMatchesFrom(FROM from)
+	{
+		return this.asList().stream().filter(match -> match.from().equals(from))
+				.toList();
+	}
+	
+	/**
+	 * @param to
+	 * @return All matches that are matched to the given TO
+	 */
+	default List<Match<FROM, TO>> allTo(TO to)
+	{
+		return this.asList().stream().filter(match -> match.to().equals(to))
+				.toList();
+	}
+	
+	
 }
