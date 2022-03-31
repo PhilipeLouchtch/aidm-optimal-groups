2214576f93e7d897bf79c815ae3641e9953091dd
Philipe Louchtch <philipelouchtch@outlook.com>
2022-03-31 21:02:27 +0200

    add Exp ProjPref generator

---
+++ src/main/java/nl/tudelft/aidm/optimalgroups/dataset/generated/prefs/ExponentiallyDistributedProjectPreferencesGenerator.java
@@ -0,0 +1,21 @@
+package nl.tudelft.aidm.optimalgroups.dataset.generated.prefs;
+
+import nl.tudelft.aidm.optimalgroups.model.project.Projects;
+import org.apache.commons.math3.distribution.ExponentialDistribution;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
+
+public class ExponentiallyDistributedProjectPreferencesGenerator extends ProjectPreferencesFromDistributionGenerator
+{
+	/**
+	 * Creates a new Linearly Distributed ProjectPreferences Generator. It generates ProjectPreferences
+	 * where in the projects rank is related to the probability of that project.
+	 * @param projects The projects for which the preferences are generated
+	 * @param chaos How neatly linear the resulting preference avg-rank profile will look. With 1 being a very clean f(x) = y linear
+	 *              line, and greater values resulting in larger std deviation. 4 looks real-ish for smaller datasets but for larger #p
+	 *              more like a line with some std dev
+	 */
+	public ExponentiallyDistributedProjectPreferencesGenerator(Projects projects, double chaos)
+	{
+		super(projects, new ExponentialDistribution(chaos));
+	}
+}
