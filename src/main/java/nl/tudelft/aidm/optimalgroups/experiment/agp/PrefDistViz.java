957cd2c1baf5a744cefc1a9df545e2f60b7deef5
Philipe Louchtch <philipelouchtch@outlook.com>
2022-09-22 16:51:49 +0200

    thesis, viz: more consistent gen prefs viz with R, not jchart

--- src/main/java/nl/tudelft/aidm/optimalgroups/experiment/agp/PrefDistViz.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/experiment/agp/PrefDistViz.java
@@ -3,14 +3,26 @@ package nl.tudelft.aidm.optimalgroups.experiment.agp;
 import nl.tudelft.aidm.optimalgroups.algorithm.GroupProjectAlgorithm;
 import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.model.PregroupingType;
 import nl.tudelft.aidm.optimalgroups.dataset.generated.GeneratedDataContext;
+import nl.tudelft.aidm.optimalgroups.dataset.generated.projprefs.ProjectPreferenceGenerator;
 import nl.tudelft.aidm.optimalgroups.experiment.agp.report.ExperimentReportInHtml;
+import nl.tudelft.aidm.optimalgroups.experiment.paper.generateddata.model.ExperimentSubResult;
 import nl.tudelft.aidm.optimalgroups.experiment.paper.generateddata.model.MinimumReqProjectAmount;
+import nl.tudelft.aidm.optimalgroups.experiment.paper.historical.comparison.setting.AnyExceptMaxCsvReport;
 import nl.tudelft.aidm.optimalgroups.model.GroupSizeConstraint;
+import nl.tudelft.aidm.optimalgroups.model.agent.Agent;
+import nl.tudelft.aidm.optimalgroups.model.dataset.DatasetContext;
+import nl.tudelft.aidm.optimalgroups.model.pref.ProjectPreference;
 import nl.tudelft.aidm.optimalgroups.model.project.Projects;
+import plouchtch.functional.actions.Rethrow;
+import plouchtch.util.Try;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static nl.tudelft.aidm.optimalgroups.experiment.paper.generateddata.predef.ProjPrefVariations.*;
 
@@ -18,7 +30,7 @@ public class PrefDistViz
 {
 	public static void main(String[] args)
 	{
-		var experimentsForInReport = new ArrayList<Experiment>();
+		var forExport = new ArrayList<Result>();
 		
 		var pregroupingType = PregroupingType.anyCliqueHardGrouped();
 
@@ -36,16 +48,89 @@ public class PrefDistViz
 			{
 				int numProjects = new MinimumReqProjectAmount(groupSize, num_students).asInt();
 				var projects = Projects.generated(numProjects, 1);
-				
-				var experiment = new Experiment(new GeneratedDataContext(num_students, projects, groupSize, pref_type.makeGeneratorFor(projects)), algorithms);
-				experimentsForInReport.add(experiment);
+
+				var pref_gen = pref_type.makeGeneratorFor(projects);
+				var generatedDataset = new GeneratedDataContext(num_students, projects, groupSize, pref_gen);
+				var sample = new Result(generatedDataset, num_students, pref_type.shortName());
+				forExport.add(sample);
 			}
 		}
 
-		new ExperimentReportInHtml(experimentsForInReport)
-			.writeHtmlSourceToFile(new File("reports/prefs_viz.html"));
+		// export to csv - all in single csv, include pref_type_name and num_students
+
+		write(forExport, new File("results/gen_prefs_export.csv"));
 
 		return;
+	}
+
+	record Result(DatasetContext datasetContext, Integer num_students, String pref_gen_name)
+	{
+		List<ProjectPreference> prefs()
+		{
+			return datasetContext.allAgents()
+					.asCollection().stream()
+					.map(Agent::projectPreference).toList();
+		}
+	}
+
+	public static void write(List<Result> results, File file)
+	{
+		var headers = List.of("num_students", "pref_gen_name", "project", "rank");
+
+
+		try (var writer = new FileWriter(file))
+		{
+			var headersAsLine = headers.stream().collect(Collectors.joining(",", "","\n"));
+			writer.write(headersAsLine);
+
+			for (var result : results)
+			{
+				result.prefs().forEach(pref -> {
+					pref.forEach((project, rank, iterControl) -> {
+
+						var NA = Integer.valueOf(0);
+
+						writeUsing(writer,
+
+							result.num_students().toString(),
+							result.pref_gen_name(),
+
+							project.name(),
+							(rank.isPresent() ? rank.asInt() : NA).toString()
+
+						);
+
+					});
+				});
+
+			}
+		}
+		catch (IOException ex)
+		{
+			throw new RuntimeException(ex);
+		}
+
+	}
+
+	public static void writeUsing(Writer writer, String... fields)
+	{
+		try
+		{
+			for (int i = 0; i < fields.length; i++)
+			{
+				var field = fields[i];
+				writer.write(field);
+
+				if (i < fields.length-1)
+					writer.write(",");
+				else
+					writer.write("\n");
+			}
+		}
+		catch (IOException exception)
+		{
+			throw new RuntimeException(exception);
+		}
 	}
 
 
